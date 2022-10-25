412d56e0bea1562185427d744ced75e84b512fde
June <jwon@galileo.io>
2022-10-25 15:53:37 -0400

    Update to add file name and accept multiple files

--- update.py
+++ update.py
@@ -1,37 +1,50 @@
+import string
 import requests
 import maya
 import datetime
 from os import environ
+from typing import Optional, List
 from dotenv import load_dotenv
 load_dotenv()
 
-def get_updates():
+def get_figma_file_name(file_key:str) -> Optional[List[str]]:
+  """Retrieves file name of figma file"""
+
   FIGMA_PERSONAL_ACCESS_TOKEN = environ.get('FIGMA_PERSONAL_ACCESS_TOKEN')
-  FIGMA_FILE_KEY = environ.get('FIGMA_FILE_KEY')
-  FIGMA_API_URL = "https://api.figma.com/v1/files/" + FIGMA_FILE_KEY + "/versions"
+  FIGMA_API_URL = f"https://api.figma.com/v1/files/{file_key}"
+  FIGMA_API_HEADERS = { 'X-FIGMA-TOKEN': FIGMA_PERSONAL_ACCESS_TOKEN }
+
+  r = requests.get(url = FIGMA_API_URL, headers = FIGMA_API_HEADERS)
+  data = r.json()
+
+  return data["name"]
+
+def get_figma_file_updates(file_key:str) -> Optional[List[str]]:
+  """Retrieves formatted message containing updates for the file"""
+
+  FIGMA_PERSONAL_ACCESS_TOKEN = environ.get('FIGMA_PERSONAL_ACCESS_TOKEN')
+  FIGMA_API_URL = f"https://api.figma.com/v1/files/{file_key}/versions"
   FIGMA_API_HEADERS = { 'X-FIGMA-TOKEN': FIGMA_PERSONAL_ACCESS_TOKEN }
 
   r = requests.get(url = FIGMA_API_URL, headers = FIGMA_API_HEADERS)
   data = r.json()
   versions = data["versions"]
 
-  filter_function = lambda x: maya.parse(x['created_at']).datetime().date() == datetime.date.today() and x['description'] is not None and len(x['description']) > 0
+  filter_function = lambda x: maya.parse(
+      x['created_at']).datetime().date() == datetime.date.today() and x['description'] is not None and len(x['description']
+    ) > 0
   todays_versions = list(filter(filter_function, versions))
-  if len(todays_versions) > 0:
-    message = format_message(todays_versions)
-    post_message(message)
 
-def format_message(todays_versions):
-  date = datetime.datetime.today()
-  message = str(date.month) + "/" + str(date.day) + "\n"
-
+  updates = []
   for version in todays_versions:
-    description = version["description"]
-    message += "\n" + description
+    if version['description']:
+      updates.append(version['description'])
 
-  return message
+  return updates
+  
+def post_message(message:str) -> None:
+  """Post message to slack"""
 
-def post_message(message):
   SLACK_TEAM_ID = environ.get('SLACK_TEAM_ID')
   SLACK_USER_ID = environ.get('SLACK_USER_ID')
   SLACK_CHANNEL_ID = environ.get('SLACK_CHANNEL_ID')
@@ -41,4 +54,29 @@ def post_message(message):
   r = requests.post(url = SLACK_API_URL, json = data)
   print(message)
 
-get_updates()
+def slack_updates_for_figma_files() -> None:
+  """Retrieves new updates figma files and posts to a slack channel"""
+
+  FIGMA_FILE_KEY = environ.get('FIGMA_FILE_KEY')
+  FIGMA_FILE_KEYS = environ.get('FIGMA_FILE_KEYS', "")
+  
+  file_keys = FIGMA_FILE_KEYS.split(',')
+
+  if FIGMA_FILE_KEY:
+    file_keys = [FIGMA_FILE_KEY]
+
+  message = ""
+  for file_key in file_keys:
+    file_name = get_figma_file_name(file_key=file_key)
+    file_updates = get_figma_file_updates(file_key=file_key)
+    if file_updates:
+      message += f"\n{file_name}"
+      for file_update in file_updates:
+        message += f"\n * {file_update}"
+  
+  if message:
+    # Attach date to message
+    todays_date = datetime.datetime.today().strftime('%Y-%m-%d')
+    post_message(f"[{todays_date}]{message}")
+
+slack_updates_for_figma_files()
