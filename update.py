2f9b47e6502d66a9a32bf11b726cdf24ce7231b2
June <jwon@galileo.io>
2023-01-16 12:57:33 -0500

    Update to using railway

--- update.py
+++ update.py
@@ -45,6 +45,12 @@ def get_figma_file_updates(file_key:str) -> Optional[List[str]]:
 def post_message(message:str) -> None:
   """Post message to slack"""
 
+  DEBUG_MODE = environ.get('DEBUG_MODE', "0")
+  print(type(DEBUG_MODE))
+  if DEBUG_MODE != "0":
+    return
+
+  return
   SLACK_TEAM_ID = environ.get('SLACK_TEAM_ID')
   SLACK_USER_ID = environ.get('SLACK_USER_ID')
   SLACK_CHANNEL_ID = environ.get('SLACK_CHANNEL_ID')
@@ -57,7 +63,7 @@ def post_message(message:str) -> None:
 
 def slack_updates_for_figma_files() -> None:
   """Retrieves new updates figma files and posts to a slack channel"""
-
+  
   FIGMA_FILE_KEY = environ.get('FIGMA_FILE_KEY')
   FIGMA_FILE_KEYS = environ.get('FIGMA_FILE_KEYS', "")
   
